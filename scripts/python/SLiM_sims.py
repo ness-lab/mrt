d858903f1fa946ee14533a34c657c4582b2d117b
James Santangelo <james.santangelo37@gmail.com>
2020-02-21 14:37:01 -0500

    Cleaned up script and changed to simple Argparse

--- scripts/python/SLiM_sims.py
+++ scripts/python/SLiM_sims.py
@@ -5,17 +5,13 @@
 # simulation to easily be run on the cluster using GNU parallel.
 
 import subprocess
-import sys
 
 
-def main():
-
-    N = int(sys.argv[1])  # initial N value
-    bottleneck = float(sys.argv[2])  # proportion of N
+def run_slim(N, bot):
 
     # Call SLiM from command line with N and bottleneck proportion values
     # (passed as command-line arguments)
-    process = subprocess.Popen(["slim", "-d", "N=" + str(N), "-d", "bottleneck=" + str(bottleneck), "../slim/Bottleneck.slim"],
+    process = subprocess.Popen(["slim", "-d", "N=" + str(N), "-d", "bot=" + str(bot), "../slim/Bottleneck.slim"],
                                stdout = subprocess.PIPE,
                                stderr = subprocess.PIPE,
                                universal_newlines = True)
@@ -23,9 +19,18 @@ def main():
 
     # out is output of slim
     # err is error message from slim
-    parse(out)  # calls function to parse the output
+    print(out)  # calls function to parse the output
     print(err)  # prints error message
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("N", help = "The desired population size", type = int)
+    parser.add_argument("bot", help = "The desired strength of the population bottleneck. Expressed as the proportion of the population sampled during the bottleneck. 1.0 = No bottleneck", type = float)
+    args = parser.parse_args()
+    N = args.N
+    bot = args.bot
+    print(N, bot)
+
+    run_slim(N, bot)
